a20226c319ca00560438fbdb2f9c36f93b1ace5b
Katie Loyd <katiecameronloyd@gmail.com>
2019-01-18 14:12:59 -0500

    Add delete, done and undone links

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    path('delete/<item_id>', views.delete, name='delete'),
+    path('done/<item_id>', views.done, name='done'),
+    path('undone/<item_id>', views.undone, name='undone')
 ]
--- todo_list/views.py
+++ todo_list/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import List
 from .forms import ListForm
 from django.contrib import messages
+from django.http import HttpResponseRedirect
 
 def home(request):
     if request.method == 'POST':
@@ -20,3 +21,21 @@ def home(request):
 def about(request):
     context = {'first_name': 'Katie', 'last_name': 'Loyd'}
     return render(request, 'about.html', context)
+
+def delete(request, item_id):
+    item = List.objects.get(pk=item_id)
+    item.delete()
+    messages.success(request, ('Item has been deleted'))
+    return redirect('home')
+
+def done(request, item_id):
+    item = List.objects.get(pk=item_id)
+    item.completed = True
+    item.save()
+    return redirect('home')
+
+def undone(request, item_id):
+    item = List.objects.get(pk=item_id)
+    item.completed = False
+    item.save()
+    return redirect('home')
